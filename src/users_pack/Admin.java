f566908c9311f3b7a01a434cf10443b170999b24
amirafathy-59 <amirafa592001@gmail.com>
2021-11-19 16:32:58 +0000

    Update Admin.java
    update Admin with SuspendUsers

--- src/users_pack/Admin.java
+++ src/users_pack/Admin.java
@@ -5,13 +5,21 @@ import storage_pack.ListStorage;
 
 public class Admin extends User{
     static List<User> pendingRegistrations;
-    
+     static List<User> suspendedUsers; // if  the admin suspended any user , this user will add to the list and cannot be able to login
+     static List<User> unsuspendedUsers; // if the admin canceled the user's suspending , this user will add to the list and able to login the system
+                                         // because if any user will login to the system , we check this user in these two lists     
     public Admin(String userName, String mobileNumber,String password,String email){
         super(userName, mobileNumber,password,email);
     }
     
     public static void addPendingRegistrations(User user){
         pendingRegistrations.add(user);
+    }
+     public static void suspendUser (User user){
+        suspendedUsers.add(user);
+    }
+       public static void unsuspendUser (User user){
+        unsuspendedUsers.add(user);
     }
     
     public boolean acceptRegistration(String userName){

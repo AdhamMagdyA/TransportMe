6b13142be89aad17f890e58ff3a53c60d32c7c52
unknown <adhammagdyz67@gmail.com>
2021-11-23 13:14:40 +0200

    added feature to store clients and drivers separately

--- src/transportme/TransportMe.java
+++ src/transportme/TransportMe.java
@@ -1,15 +1,20 @@
 package transportme;
 
 import java.util.List;
-import users_pack.User;
+import users_pack.*;
 
 public class TransportMe {
 
     static User loggedIn = null;
     static List<User> registeredUsers = null;
+    static List<Driver> drivers = null;
+    static List<Client> clients = null;
     
     public static void storeUser(User u){
         registeredUsers.add(u);
+        if(u instanceof Driver) drivers.add((Driver) u);
+        if(u instanceof Client) clients.add((Client) u);
+
         System.out.println("User is successfully registered");
     }
     
